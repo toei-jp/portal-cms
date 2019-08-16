7d41923af7001b5d654255cbbd39ef7859b832c5
Atsushi Okui <okui@motionpicture.jp>
2019-08-16 12:32:35 +0900

    ディレクトリのタイポ修正

---
+++ src/Middleware/AbstractMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * AbstractMiddleware.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+namespace Toei\PortalAdmin\Middleware;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Abstract middleware class
+ */
+abstract class AbstractMiddleware
+{
+    /** @var ContainerInterface */
+    protected $container;
+    
+    /**
+     * construct
+     *
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+}
